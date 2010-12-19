297561d3e68b43ba77a44c73247afa9f80062e13
cardih <cardih@gmail.com>
2010-12-19 15:18:53 -0500

    client

--- main.cpp
+++ main.cpp
@@ -8,23 +8,33 @@
  */
 
 #include "kserversocketfactory.h"
+#include "kunixsocketadapter.h"
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    KServerSocketAdapter* server = 
-        KServerSocketFactory::getSocketAdapter();
+//    KServerSocketAdapter* server = 
+//        KServerSocketFactory::getSocketAdapter();
     
     const int buflen = 512;
     byte buf[buflen];
     strcpy(buf, "hello, world");
     
-    if (server->accept()) 
-        while (1) {
-            server->send(buf, strlen(buf) + 1);
-            if (server->receive(buf, buflen))
-                cout << "received " << buf << endl;
-        }
+//    if (server->accept()) 
+//        while (1) {
+//            server->send(buf, strlen(buf) + 1);
+//            if (server->receive(buf, buflen))
+//                cout << "received " << buf << endl;
+//        }
+    
+    KUnixSocketAdapter socket;
+    socket.connect("10.0.1.7", 22222);
+    while (1) {
+        if (socket.receive(buf, buflen))
+            cout << "received " << buf << endl;
+        gets(buf);
+        socket.send(buf, strlen(buf) + 1);
+    }
 }
 
